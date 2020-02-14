62e255574fc7e56b1f9477a49916cac05306d206
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-02-14 08:54:55 +0100

    Finnished filter and reverse

--- exercicios/e4_manipulacion_de_listas/lib/manipulating.ex
+++ exercicios/e4_manipulacion_de_listas/lib/manipulating.ex
@@ -1,2 +1,57 @@
 defmodule Manipulating do
+    def filter([], _) do
+        []
+    end
+
+    def filter([h | t], n) do
+        if h <= n do
+            [h | filter(t, n)]
+        else
+            filter(t, n)
+        end
+    end
+
+    def reverse(l) do
+        reverse(l, [])
+    end
+
+    defp reverse([], acc) do
+        acc
+    end
+
+    defp reverse([h | t], acc) do
+        reverse(t, [h | acc])
+    end
+
+    defp append([], []) do
+        []
+    end
+
+    defp append(l, []) do
+        l
+    end
+
+    defp append([], l) do
+        l
+    end
+
+    defp append([h | t], l) do
+        [h | append(t, l)]
+    end
+
+    def concatenate(l) do
+        concatenate(l, [])
+    end
+
+    defp concatenate([], laux) do
+        laux
+    end
+
+    defp concatenate([h], laux) do
+        append(laux, [h])
+    end
+
+    defp concatenate([h | t], laux) do
+        concatenate(t, append(laux, [h]))
+    end
 end
