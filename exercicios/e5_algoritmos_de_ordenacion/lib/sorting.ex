48cca3b56cecc815bc907afb5b132efd2a2866eb
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-02-18 14:17:08 +0100

    Implemented quicksort

--- exercicios/e5_algoritmos_de_ordenacion/lib/sorting.ex
+++ exercicios/e5_algoritmos_de_ordenacion/lib/sorting.ex
@@ -1,2 +1,29 @@
 defmodule Sorting do
+    def quicksort([]) do
+        []
+    end
+
+    def quicksort([h | t]) do
+        quicksort(h, t, [], [])
+    end
+
+    defp quicksort(piv, [], lless, lmore) do
+        lless ++ [piv | lmore]
+    end
+
+    defp quicksort(piv, [h], lless, lmore) do
+        if h < piv do
+            quicksort(lless ++ [h]) ++ [piv | quicksort(lmore)]
+        else
+            quicksort(lless) ++ [piv | quicksort(lmore ++ [h])]
+        end
+    end
+
+    defp quicksort(piv, [h | t], lless, lmore) do
+        if h < piv do
+            quicksort(piv, t, lless ++ [h], lmore)
+        else
+            quicksort(piv, t, lless, lmore ++ [h])
+        end
+    end
 end
