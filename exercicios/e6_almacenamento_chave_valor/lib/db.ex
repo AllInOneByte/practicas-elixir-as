4acbea4448a4ee6958dc9e81853d0e5db50296c7
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-02-19 09:51:13 +0100

    Implemented all the funtions except destroy/1, match/2 needs a fix

--- exercicios/e6_almacenamento_chave_valor/lib/db.ex
+++ exercicios/e6_almacenamento_chave_valor/lib/db.ex
@@ -1,2 +1,30 @@
 defmodule Db do
+    def new() do
+        []
+    end
+
+    def write(db_ref, key, element) do
+       [db_ref | [[key, element]]] 
+    end
+
+    def delete(db_ref, key) do
+        for [k, v] when k != key <- db_ref, do: [k, v]
+    end
+
+    def read(db_ref, key) do
+        element = for [k, v] when k == key <- db_ref, do: v
+        if element != [] do
+            {:ok, hd(element)}
+        else
+            {:error, :not_found}
+        end
+    end
+
+    def match(db_ref, element) do
+        for [k, v] when v == element <- db_ref, do: k
+    end
+
+    def destroy(_db_ref) do
+        :ok
+    end
 end
