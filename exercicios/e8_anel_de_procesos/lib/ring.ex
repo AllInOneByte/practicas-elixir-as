024d2262df317a3eeb327b6f1e1e91192b9d14bc
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-02-20 20:17:59 +0100

    Implemented e8, it works when it wants

--- exercicios/e8_anel_de_procesos/lib/ring.ex
+++ exercicios/e8_anel_de_procesos/lib/ring.ex
@@ -1,2 +1,28 @@
 defmodule Ring do
+    def start(n, m, msg) do
+        next = spawn(Ring, :conf, [n - 1, self()])
+        send(next, {m - 1, msg})
+        listen(next)
+    end
+
+    def conf(0, origin) do
+        listen(origin)
+    end
+
+    def conf(n, origin) when n > 0 do
+        listen(spawn(Ring, :conf, [n - 1, origin]))
+    end
+
+    def listen(next) do
+        receive do
+            {:m_zero, :die} -> 
+                send(next, {:m_zero, :die})
+                :ok
+            {0, _msg} ->
+                send(next, {:m_zero, :die})
+            {m, msg} ->
+                send(next, {m - 1, msg})
+                listen(next)
+        end
+    end
 end
